cea576371571762b06e1803363806f53c9fbcfd1
Poojith Sureshkumar <pjsureshkumar123@gmail.com>
2025-11-04 14:56:13 -0600

    Add files via upload

---
+++ randoop/RegressionTest0.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest00.class, RegressionTest01.class, RegressionTest02.class, RegressionTest03.class })
+public class RegressionTest0 {
+}
+
